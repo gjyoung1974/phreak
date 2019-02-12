debbe97bc7177482d26ef003d2bfef38aa9b87a4
Gordon Young <gordon@scalesec.com>
2019-02-11 18:50:00 -0700

    deleted:    dtmf-tones.iml 	deleted:    out/production/dtmf-tones/META-INF/dtmf-tones.kotlin_module 	deleted:    out/production/dtmf-tones/com/gyoung/demo/audio/dialer.class 	deleted:    out/production/dtmf-tones/com/gyoung/demo/audio/dtmf.class 	deleted:    out/production/dtmf-tones/com/gyoung/demo/audio/payphone.class 	modified:   src/com/gyoung/demo/audio/dialer.java

--- src/com/gyoung/demo/audio/dialer.java
+++ src/com/gyoung/demo/audio/dialer.java
@@ -9,11 +9,11 @@ public class dialer {
          */
 
         try {
-            dtmf.generateTones(697, 1209, 100, 20);
-
-//            payphone.dime();
-//            payphone.nickle();
-//            payphone.qaurter();
+            dtmf.generateTones(350, 440, 5000, 10);
+            dtmf.generateTones(770, 1209, 100, 10);
+            //            payphone.dime();
+            //            payphone.nickle();
+            //            payphone.qaurter();
 
         } catch (Exception e) {
             e.printStackTrace();
